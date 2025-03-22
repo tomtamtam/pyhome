de829ec9dc055d92e48945b706200b87a98a4077
tom <iwan.tom@proton.me>
2025-03-22 15:02:18 +0100

    1

--- HomeGPIOControll.py
+++ HomeGPIOControll.py
@@ -3,12 +3,12 @@ import string
 import time
 import paho.mqtt.client as mqtt
 
-
-jalousineNames = ["zimmerTom", "zimmerElla", "schlafZimmer", "badBraun", "wohnZimmerGarten", "WohnZimmerStraße","hauswirtschaftsraum", "kücheGarten", "kücheStraße", "büroGarten", "büroStraße"]
+# Liste der Jalousienamen und deren Dauerzeiten
+jalousineNames = ["zimmerTom", "zimmerElla", "schlafZimmer", "badBraun", "wohnZimmerGarten", "WohnZimmerStraße", "hauswirtschaftsraum", "kücheGarten", "kücheStraße", "büroGarten", "büroStraße"]
 jalousineDurationTimes = [20, 40, 40, 10, 60, 10, 10, 10, 10, 40, 10]
 currentPercentages = [100.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00]
 
-
+# Funktion zum Simulieren des GPIO-Pulsierens
 def pulse_pin(pin: int, duration: float = 0.1):
     #GPIO.setmode(GPIO.BCM)  # BCM-Nummerierung verwenden
     #GPIO.setup(pin, GPIO.OUT)
@@ -18,91 +18,80 @@ def pulse_pin(pin: int, duration: float = 0.1):
     #GPIO.output(pin, GPIO.LOW)
 
     #GPIO.cleanup()  # GPIO-Pins zurücksetzen
-    print("pin "+str(pin)+"was set")
+    print(f"Pin {pin} wurde für {duration} Sekunden aktiviert")
 
-
-# Beispielaufruf:
-# pulse_pin(17, 0.5)  # Schaltet GPIO 17 für 0.5 Sekunden an
-
-
+# Funktion zum Ändern der Jalousienposition
 def change_jalousine(jalousineName: string, percentage: float):
-    # set variables
+    # Variablen initialisieren
     jalousineNumb = 0
     percentageDifference = 0.00
     dir = False
     dirString = ""
     pinNumb = 0
 
-
-    for i, name in enumerate(jalousineNames):
-        if jalousineName == name:
-                jalousineNumb = i
-                break
+    # Finde den Index der Jalousine in der Liste
     if jalousineName not in jalousineNames:
         print("Jalousie nicht gefunden!")
         return
-
-    # set difference between old and new percentage
-    if currentPercentages[jalousineNumb] >= percentage:
-        percentageDifference = currentPercentages[jalousineNumb] - percentage
     else:
-        percentageDifference = percentage - currentPercentages[jalousineNumb]
+        jalousineNumb = jalousineNames.index(jalousineName)
 
-    # set durationtime between old and new percentage
-    print(percentageDifference)
-    print(jalousineDurationTimes[jalousineNumb])
-    durationTime = jalousineDurationTimes[jalousineNumb] * (percentageDifference/100)
+    # Berechne die Differenz zwischen alter und neuer Position
+    percentageDifference = abs(currentPercentages[jalousineNumb] - percentage)
 
+    # Berechne die Dauer der Bewegung
+    durationTime = jalousineDurationTimes[jalousineNumb] * (percentageDifference / 100)
 
-
-    # set direction to move direction
+    # Bestimme die Richtung der Bewegung
     if percentage < currentPercentages[jalousineNumb]:
         dir = False
         dirString = "down"
-        print("set directiont to:" + str(dirString))
     elif percentage > currentPercentages[jalousineNumb]:
         dir = True
         dirString = "up"
-        print("set directiont to:" + str(dirString))
     else:
-        print("same position at direction calculation")
+        print("Jalousie bereits in der gewünschten Position")
+        return
 
-
-    # set pin to set calculating pin for upmoving and downmoving
-    if dir == True:
+    # Setze die Pin-Nummer basierend auf der Richtung
+    if dir:
         pinNumb = jalousineNumb
-        print("set jalousine " + str(jalousineNames[jalousineNumb]) + " to " + str(percentage) + " pin = " + str(pinNumb))
     else:
-        pinNumb = jalousineNumb+1
-        print("set jalousine " + str(jalousineNames[jalousineNumb]) + " to " + str(percentage) + " direction: " + str(dirString) + " pin: " + str(pinNumb))
+        pinNumb = jalousineNumb + 1
 
-    # set pin at chosen position foe 0.1 seconds
+    # Aktiviere den Pin für die Bewegung
     pulse_pin(pinNumb, 0.01)
-    print("pin :"+str(pinNumb)+"was set first time moving "+(dirString)+"wards durationTime: "+str(durationTime))
+    print(f"Jalousie {jalousineNames[jalousineNumb]} wird auf {percentage}% gestellt. Richtung: {dirString}, Pin: {pinNumb}, Dauer: {durationTime} Sekunden")
 
-    # set timer as long as jalousine is moving
+    # Warte, bis die Bewegung abgeschlossen ist
     time.sleep(durationTime)
 
-    #set to false
-    pulse_pin(jalousineNumb, 0.01)
-    print("pin :"+str(pinNumb)+"was set second time stopping moving "+(dirString)+"wards")
+    # Deaktiviere den Pin, um die Bewegung zu stoppen
+    pulse_pin(pinNumb, 0.01)
+    print(f"Jalousie {jalousineNames[jalousineNumb]} hat die gewünschte Position erreicht.")
 
+    # Aktualisiere die aktuelle Position der Jalousine
+    currentPercentages[jalousineNumb] = percentage
 
-
+# Callback-Funktion für MQTT-Nachrichten
 def on_message(client, userdata, msg):
     print(f"Empfangenes Topic: {msg.topic}, Nachricht: {msg.payload.decode()}")
     msgTopic = msg.topic
-    jalIndex = 4000
+    jalIndex = 40000
     jalName = ""
     jalState = int(msg.payload.decode().split(".")[0])
-    if msgTopic[:8]== "Jalousie":
-        jalIndex = msgTopic[-1]
-        jalName = jalousineNames[jalIndex]
-        print("jalousine: "+jalIndex)
 
-        change_jalousine(jalName,jalState/100)
-    #change_jalousine()
+    if msgTopic[:8] == "Jalousie":
+        try:
+            jalIndex = int(msgTopic[-1])  # Konvertiere den letzten Teil des Topics in einen Integer
+            jalName = jalousineNames[jalIndex]
+            print(f"Jalousine: {jalIndex}")
 
+            change_jalousine(jalName, jalState / 100)
+        except (ValueError, IndexError):
+            print("Ungültiger Index oder Konvertierungsfehler im Topic")
+
+# MQTT-Client initialisieren und verbinden
 client = mqtt.Client()
 client.on_message = on_message
 
@@ -111,13 +100,5 @@ client.connect("192.168.178.144", 1883, 60)
 # Alle Topics abonnieren (Wildcard #)
 client.subscribe("#")
 
+# Endlosschleife für die MQTT-Kommunikation
 client.loop_forever()
-
-
-
-
-
-
-
-
-
